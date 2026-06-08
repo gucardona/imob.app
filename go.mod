1575e7b2f901194849b6128f866293baa10a50b2
Gustavo Parcianello Cardona <gupcardona@gmail.com>
2026-06-08 17:16:48 +0000

    feat: add image resize pipeline producing thumb and grande JPEG variants

--- go.mod
+++ go.mod
@@ -4,21 +4,21 @@ go 1.26.4
 
 require (
 	github.com/a-h/templ v0.3.1020
+	github.com/disintegration/imaging v1.6.2
+	golang.org/x/crypto v0.52.0
+	golang.org/x/term v0.43.0
 	modernc.org/sqlite v1.34.0
 )
 
 require (
-	github.com/disintegration/imaging v1.6.2 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
-	golang.org/x/crypto v0.52.0 // indirect
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
 	golang.org/x/sys v0.45.0 // indirect
-	golang.org/x/term v0.43.0 // indirect
 	modernc.org/gc/v3 v3.0.0-20240107210532-573471604cb6 // indirect
 	modernc.org/libc v1.55.3 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
---
+++ internal/images/images.go
@@ -0,0 +1,72 @@
+package images
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"os"
+	"path/filepath"
+
+	"github.com/disintegration/imaging"
+)
+
+const (
+	thumbWidth  = 400
+	grandeWidth = 1600
+	jpegQuality = 85
+)
+
+// Paths holds the on-disk file names (relative to the destination directory)
+// of the three variants produced for an uploaded photo.
+type Paths struct {
+	Original string
+	Thumb    string
+	Grande   string
+}
+
+// SaveVariants decodes image data, writes the original (re-encoded as JPEG for
+// consistency) plus "thumb" (~400px wide) and "grande" (~1600px wide) resized
+// variants into destDir, named "<baseName>-<variant>.jpg". Images narrower than
+// a variant's target width are kept at their original size (no upscaling).
+func SaveVariants(data []byte, destDir, baseName string) (Paths, error) {
+	img, err := imaging.Decode(bytes.NewReader(data), imaging.AutoOrientation(true))
+	if err != nil {
+		return Paths{}, fmt.Errorf("decoding image: %w", err)
+	}
+
+	paths := Paths{
+		Original: baseName + "-original.jpg",
+		Thumb:    baseName + "-thumb.jpg",
+		Grande:   baseName + "-grande.jpg",
+	}
+
+	if err := saveResized(img, destDir, paths.Original, img.Bounds().Dx()); err != nil {
+		return Paths{}, err
+	}
+	if err := saveResized(img, destDir, paths.Thumb, thumbWidth); err != nil {
+		return Paths{}, err
+	}
+	if err := saveResized(img, destDir, paths.Grande, grandeWidth); err != nil {
+		return Paths{}, err
+	}
+
+	return paths, nil
+}
+
+func saveResized(img image.Image, destDir, fileName string, targetWidth int) error {
+	resized := img
+	if img.Bounds().Dx() > targetWidth {
+		resized = imaging.Resize(img, targetWidth, 0, imaging.Lanczos)
+	}
+
+	if err := os.MkdirAll(destDir, 0o755); err != nil {
+		return fmt.Errorf("creating directory %s: %w", destDir, err)
+	}
+
+	dest := filepath.Join(destDir, fileName)
+	if err := imaging.Save(resized, dest, imaging.JPEGQuality(jpegQuality)); err != nil {
+		return fmt.Errorf("saving %s: %w", dest, err)
+	}
+
+	return nil
+}
